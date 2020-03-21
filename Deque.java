f24ea17334c29aad6360b38e07f259a4fbf7da6d
Kutuzov Vlad <alphagrimsaw@gmail.com>
2020-03-21 02:30:10 +0200

    First one

---
+++ Deque.java
@@ -0,0 +1,236 @@
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+    private Node first, last;
+    private int size;
+
+    private class Node {
+        Item item;
+        Node next;
+        Node prev;
+    }
+
+    // construct an empty deque
+    public Deque() {}
+
+    // is the deque empty?
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // return the number of items on the deque
+    public int size() {
+        return size;
+    }
+
+    // add the item to the front +++
+    public void addFirst(Item item) {
+        if (item == null)
+            throw new IllegalArgumentException("adding null");
+
+        Node newFirst = new Node();
+
+        newFirst.item = item;
+        newFirst.prev = null;
+
+        newFirst.next = first;
+
+        // making first
+        first = newFirst;
+        if (isEmpty()) last = first;
+        size++;
+    }
+
+    // add the item to the back
+    public void addLast(Item item) {
+        if (item == null)
+            throw new IllegalArgumentException("adding null");
+
+        // makin' last prev
+        Node oldLast = last;
+        last = new Node();
+
+        last.item = item;
+        last.next = null;
+        last.prev = oldLast;
+
+        if (isEmpty()) first = last;
+        else oldLast.next = last;
+        size++;
+    }
+
+    // remove and return the item from the front
+    public Item removeFirst() {
+        if(isEmpty())
+            throw new java.util.NoSuchElementException("remove from empty deque");
+
+        Item item = first.item;
+
+        // here if sz == 1 then here
+        // all pointers makes null
+        first = first.next;
+        size--;
+
+        if (isEmpty()) last = null;
+        return item;
+    }
+
+    // remove and return the item from the back
+    public Item removeLast() {
+        if(isEmpty())
+            throw new java.util.NoSuchElementException("remove from empty deque");
+
+        Item item = last.item;
+        last = last.prev;
+
+        //last.next = null;
+        size--;
+        if (isEmpty()) first = null;
+        return item;
+    }
+
+    // return an iterator over items in order from front to back
+    public Iterator<Item> iterator() {
+        return new DequeIterator();
+    }
+
+    private class DequeIterator implements Iterator<Item> {
+        private Node current;
+
+        public DequeIterator() {
+            current = first;
+        }
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public Item next() {
+            if(!hasNext())
+                throw new java.util.NoSuchElementException("no more items to return");
+
+            Item item = current.item;
+            current = current.next;
+            return item;
+
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+    }
+
+
+    // unit testing (required)
+    public static void main(String[] args) {
+        Deque<String> sample = new Deque<>();
+
+
+        // First pack ----------
+        System.out.println("First pack of tests");
+        // ***
+        // first first
+        sample.addFirst("helloworld");
+        String tmp = sample.removeFirst();
+        System.out.println(tmp);
+        // last first
+        sample.addLast("helloworld");
+        tmp = sample.removeFirst();
+        System.out.println(tmp);
+        // first last
+        sample.addFirst("helloworld");
+        tmp = sample.removeLast();
+        System.out.println(tmp);
+        // last last
+        sample.addLast("helloworld");
+        tmp = sample.removeLast();
+        System.out.println(tmp);
+
+
+        // Second pack ----------
+        System.out.println();
+        System.out.println("Second pack of tests");
+        // ***
+        // 1 3 2
+        sample.addLast("1");
+        sample.addLast("2");
+        sample.addLast("3");
+        tmp = sample.removeFirst();
+        System.out.println(tmp);
+        tmp = sample.removeLast();
+        System.out.println(tmp);
+        tmp = sample.removeLast();
+        System.out.println(tmp);
+
+        System.out.println();
+        // 1 2 3
+        sample.addLast("1");
+        sample.addLast("2");
+        sample.addLast("3");
+        tmp = sample.removeFirst();
+        System.out.println(tmp);
+        tmp = sample.removeFirst();
+        System.out.println(tmp);
+        tmp = sample.removeFirst();
+        System.out.println(tmp);
+
+        System.out.println();
+        // 3 2 1
+        sample.addLast("1");
+        sample.addLast("2");
+        sample.addLast("3");
+        tmp = sample.removeLast();
+        System.out.println(tmp);
+        tmp = sample.removeLast();
+        System.out.println(tmp);
+        tmp = sample.removeLast();
+        System.out.println(tmp);
+
+
+        // Third pack ----------
+        System.out.println();
+
+        System.out.println("Size:" + sample.size());
+
+        // -------
+        for (int i = 0; i < 10; ++i) {
+            sample.addFirst(String.valueOf(i));
+        }
+
+        System.out.println("Size:" + sample.size());
+
+        System.out.println(sample.size());
+        for (String s : sample) {
+            System.out.println(s);
+        }
+        System.out.println();
+        for (int i = 0; i < 10; ++i) {
+            System.out.println(sample.removeFirst());
+        }
+
+        System.out.println("Size:" + sample.size());
+
+        // everything was deleted
+
+        // -------
+        for (int i = 0; i < 10; ++i) {
+            sample.addLast(String.valueOf(i));
+        }
+
+        System.out.println("Size:" + sample.size());
+
+        System.out.println();
+        for (String s : sample) {
+            System.out.println(s);
+        }
+
+        System.out.println("Size:" + sample.size());
+
+        for (int i = 0; i < 10; ++i) {
+            System.out.println(sample.removeFirst());
+        }
+
+        System.out.println("Size:" + sample.size());
+    }
+}
---
+++ RandomizedQueue.java
@@ -0,0 +1,109 @@
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.Iterator;
+
+// implementation uses ResizingArray
+// TODO: tell the link to book, which implementation has been adopted
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    private Item[] s;
+    private int size = 0;
+
+
+    // construct an empty randomized queue
+    public RandomizedQueue() {
+        s = (Item[]) new Object[1];
+    }
+
+    // is the randomized queue empty?
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // return the number of items on the randomized queue
+    public int size() {
+        return size;
+    }
+
+    // add the item
+    public void enqueue(Item item) {
+        // using resizing array semantics
+        if (size == s.length) resize(2 * s.length);
+
+        // find random empty position to fill
+        int pos = StdRandom.uniform(s.length);
+        while (s[pos] != null)
+            pos = StdRandom.uniform(s.length);
+
+        s[pos] = item;
+        ++size;
+    }
+
+    // remove and return a random item
+    public Item dequeue() {
+        // resizing too large array
+        if (size > 0 && size == s.length / 4) resize(s.length / 2);
+
+        // detecting non-empty position
+        int pos = StdRandom.uniform(s.length);
+        while (s[pos] == null) {
+            pos = StdRandom.uniform(s.length);
+        }
+
+        Item item = s[pos];
+        s[pos] = null;
+        --size;
+        return item;
+    }
+
+    // return a random item (but do not remove it)
+    public Item sample() {
+        // detecting non-empty position
+        int pos = StdRandom.uniform(s.length);
+        while (s[pos] == null) {
+            pos = StdRandom.uniform(s.length);
+        }
+        return s[pos];
+    }
+
+    private void resize(int capacity) {
+        Item[] copy = (Item[]) new Object[capacity];
+        if (capacity < s.length) StdRandom.shuffle(s, 0, capacity);
+        System.arraycopy(s, 0, copy, 0, capacity);
+        s = copy;
+    }
+
+
+    // return an independent iterator over items in random order
+    public Iterator<Item> iterator() { return new RandomizedQueueIterator();}
+
+    private class RandomizedQueueIterator implements Iterator<Item> {
+        // "randomized" cuz each time we start iteration
+        // from randomly chosen pos and the iterates
+        // to the right
+
+        private int sz = size;
+
+        // unique starting position for each iterator
+        private int pos = StdRandom.uniform(s.length);
+
+        public boolean hasNext() {
+            return sz > 0;
+        }
+
+        public Item next() {
+            if(!hasNext())
+                throw new java.util.NoSuchElementException("no more items to return");
+
+            // choosing position with non-empty containing
+            while (s[pos] == null)
+                pos = (pos+1) % s.length;
+            --sz;
+            return s[pos];
+
+        }
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+    }
+}
